9dda1d209ba321bb1a21fdb56e1b7e93b7d58eab
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2021-10-31 21:34:05 +0100

    wip

---
+++ packet/packet_reader.v
@@ -0,0 +1,22 @@
+module packet.reader
+
+pub fn read_varint() {
+	mut value := 0
+    mut bitOffset := 0
+    mut currentByte := byte(0)
+
+	for ((currentByte & 0b10000000) != 0) {
+		if (bitOffset == 5) panic("VarInt is too big")
+
+        currentByte = readByte()
+        value |= (currentByte & 0b01111111) << bitOffset;
+
+        ++bitOffset
+	}
+
+    return value;
+}
+
+pub fn read_byte() {
+	return byte
+}
