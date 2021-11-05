f27b0e3566eb772a9d766c5ae6b6d3bbc85b081c
mrsherobrine <mrsherobrinenaomi@gmail.com>
2021-11-05 11:06:15 +0100

    add windows build script and numerous other things

--- main.v
+++ main.v
@@ -1,5 +1,24 @@
 module main
 
+import os
+import log
+
+import util
+
 fn main() {
-	println('welcome to valk')
+
+	mut logger := log.Log{
+		level: log.Level.info
+		output_target: log.LogTarget.console
+	}
+
+	logger.info('welcome to valk!')
+	logger.info('getting basic system diagnostics...')
+	logger.info('current os: ${os.user_os()}')
+	logger.info('check if all files exist...')
+	if util.setup_file_structure() {
+		logger.info('created files!')
+	} 
+	logger.info('looks good! starting...')
+
 }
