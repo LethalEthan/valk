19e45420bce18ee9d9c53426a1bf0951c212e928
mrsherobrine <mrsherobrinenaomi@gmail.com>
2021-11-07 02:47:10 +0100

    i'm so done

--- main.v
+++ main.v
@@ -33,6 +33,7 @@ fn main() {
 fn accept_conn(mut server server.Server, mut logger log.Log) {
 	for {
 		mut conn := server.tcp.accept() or { panic('could not receive connection') }
+		conn.set_blocking(true) or { panic('could not set connection to blocking') }
 		addr := conn.addr() or { panic('unable to get address from connection') }
 		logger.info('connection received from $addr')
 		handle_conn(mut conn, mut logger)
@@ -41,12 +42,14 @@ fn accept_conn(mut server server.Server, mut logger log.Log) {
 
 fn handle_conn(mut conn net.TcpConn, mut logger log.Log) {
 	mut buf := []byte{cap: 2097151}
+	mut byte_counter := 0
 	for {
-		buf << byte(conn.read(mut buf) or { 
-				logger.error('can not read packet, skipping...')
-				panic(buf.str())
-			})
-
-		logger.info('read: ${buf.str()}}')
+		byte_counter = conn.read(mut buf) or { 
+			logger.error('can not read packet, skipping...')
+			panic('${buf.str()}, ${byte_counter.str()}')
+		}
+		logger.info('read: ${buf.str()}, $byte_counter bytes large')
 	}
+	buf.clear()
+	byte_counter = 0
 }
