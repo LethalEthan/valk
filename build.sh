5464010ca0485c862667a93e69d857b8a3946978
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2021-11-08 22:21:13 +0100

    add -gc boehm to build files

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 if [[ -d "./test/" ]]
 then
-    v . -cc gcc -o test/valk -prod
+    v . -cc gcc -o test/valk -prod -show-timings -gc boehm
 else 
     mkdir test
-    v . -cc gcc -o test/valk -prod
+    v . -cc gcc -o test/valk -prod -show-timings -gc boehm
 fi
